d5dcbd0bc8e0199068d45007426df6b48aa05bab
Adomiraruu <samaranayake.rishen@gmail.com>
2024-02-07 11:28:02 +0530

    Added placeholder data

---
+++ src/placeholder-data/useradata.js
@@ -0,0 +1,84 @@
+const userData = [
+    {
+      "id": "202200011",
+      "name": "A. Patel",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "08:10",
+      "check-out": "11:00"
+    },
+    {
+      "id": "202200012",
+      "name": "B. Thompson",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "10:10",
+      "check-out": "13:00"
+    },
+    {
+      "id": "202200013",
+      "name": "C. Garcia",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "13:10",
+      "check-out": "16:00"
+    },
+    {
+      "id": "202200014",
+      "name": "D. Lee",
+      "date": "2024-02-07",
+      "status": "Absent",
+      "check-in": "-",
+      "check-out": "-"
+    },
+    {
+      "id": "202200015",
+      "name": "E. Johnson",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "08:20",
+      "check-out": "11:10"
+    },
+    {
+      "id": "202200016",
+      "name": "F. Hernandez",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "10:20",
+      "check-out": "13:10"
+    },
+    {
+      "id": "202200017",
+      "name": "G. Nguyen",
+      "date": "2024-02-07",
+      "status": "Absent",
+      "check-in": "-",
+      "check-out": "-"
+    },
+    {
+      "id": "202200018",
+      "name": "H. Smith",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "13:20",
+      "check-out": "16:10"
+    },
+    {
+      "id": "202200019",
+      "name": "I. Martin",
+      "date": "2024-02-07",
+      "status": "Present",
+      "check-in": "08:30",
+      "check-out": "11:20"
+    },
+    {
+      "id": "202200020",
+      "name": "J. Davis",
+      "date": "2024-02-07",
+      "status": "Absent",
+      "check-in": "-",
+      "check-out": "-"
+    }
+]
+
+export default userData;
