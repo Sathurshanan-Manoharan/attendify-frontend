8a7d3a059ed048d834b5b5df2d0bb71c460423f3
Adomiraruu <samaranayake.rishen@gmail.com>
2024-02-11 23:27:59 +0530

    Added provision for font change across components on page, Added secondary card (Non functional)

--- src/pages/SessionAttendance.jsx
+++ src/pages/SessionAttendance.jsx
@@ -9,15 +9,26 @@ import FormControl from "@mui/material/FormControl";
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from "@mui/material/IconButton";
 import Select from "@mui/material/Select";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+//import 'typeface-inter';
 
+/* const textFont = createTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
+});
+ */
 function SessionAttendance() {
 
   return (
+    //<ThemeProvider theme={textFont}>
     <div>
+      <div style={{ marginBottom: '10px'}}>
       <Typography variant="h4" color="#004AAD" fontWeight="bold" >Session Attendance</Typography>
       <br></br>
 
-      <Card variant="elevation" sx={{boxShadow: '0px 0px 20px 10px rgba(0, 0, 0, 0.035)', borderRadius: '20px', }}>
+      <Card variant="elevation" sx={{boxShadow: '40px 0px 20px 10px rgba(0, 0, 0, 0.035)', borderRadius: '14px', }}>
         <CardContent sx={{paddingBottom:'2px'}}>
           <Grid container spacing={2} alignItems="center" >
             <Grid item xs={5.6} >
@@ -27,7 +38,7 @@ function SessionAttendance() {
                   InputProps={{endAdornment: (<IconButton><SearchIcon /></IconButton>), disableUnderline: true,}}
                   fullWidth
                   sx={{'& .MuiFilledInput-root': {
-                    borderRadius: '20px',},
+                    borderRadius: '6px',},
                       width:'400pt'}}></TextField>
               </FormControl>
             </Grid>
@@ -58,9 +69,23 @@ function SessionAttendance() {
           </Grid>
         </CardContent>
       </Card>
+      </div>
 
+      <div>
+        <Card variant="elevation" sx={{boxShadow: '0px 0px 20px 10px rgba(0, 0, 0, 0.035)', borderRadius: '14px', }}>
+          <CardContent>
+            <Typography variant="h6" color="#004AAD" fontWeight="bold" >Attendance Overview</Typography>
+            <hr></hr>
+
+
+          </CardContent>
+        </Card>
+      </div>
     </div>
+
     
+
+    //</ThemeProvider>
     
   )
 }
