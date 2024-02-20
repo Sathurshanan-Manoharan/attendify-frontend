bc54d5e1b2a2c78ee26ec64c9f863b52945c2839
Nethmini Apsara <143925696+nethmini22@users.noreply.github.com>
2024-02-20 21:01:30 +0530

    Updated Login page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -8,19 +8,18 @@ import { styled } from '@mui/system';
 
 const LeftColumnPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: '#004AAD',
-  // padding: '-250px',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  height: '950px'
+  height: '100vh'
 
 }));
 
 const RightColumnPaper = styled(Paper)(({ theme }) => ({
   backgroundImage: `url('../src/assets/Group 17.png')`, // Replace with your actual image path
   backgroundSize: 'cover',
-  height: '950px',
+  height: '100vh',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
@@ -31,10 +30,6 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-
-
-    
   };
 
   return (
@@ -42,9 +37,9 @@ const Login = () => {
       <Grid container>
       <Grid item sx={{ position: 'absolute', top: 0, left: 0, height: '50%' , bottom:0,width:'40%'}}>
       <LeftColumnPaper>
-      <img src="../src/assets/logo.png" alt="Left Column Image" style={{ maxWidth: '100%', maxHeight: '100%', marginTop: '2%' }} />
-      <img src="../src/assets/picture.png" alt="Left Column Image" style={{ maxWidth: '80%', maxHeight: '80%', marginTop: '-10%' }} />
-      <Typography sx={{ fontSize:20,color: 'white', marginTop: '15%' }}> Streamlining Attendance, Empowering Efficiency!</Typography>
+      <img src="../src/assets/logo.png" alt="Left Column Image" style={{ maxWidth: '70%', maxHeight: '70%', marginTop: '-70px' }} />
+      <img src="../src/assets/picture.png" alt="Left Column Image" style={{ maxWidth: '60%', maxHeight: '60%', marginTop: '-15%' }} />
+      <Typography style={{ fontSize:20,color: 'white', marginTop: '5%' }}> Streamlining Attendance, Empowering Efficiency!</Typography>
 
         </LeftColumnPaper>
       </Grid>
@@ -52,7 +47,7 @@ const Login = () => {
         {/* Right Column */}
         <Grid item sx={{ position: 'absolute', top:0, right: 0, height:'100%' ,width:'60%',bottom:0}}>
           <RightColumnPaper>
-            <Typography variant="h2" sx={{ fontWeight: 'bold', color: '#004AAD',paddingTop:'20px' }}>Welcome Back!</Typography>
+          <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#004AAD',paddingTop:'30px' }}>Welcome Back!</Typography>
             <Typography >Unlock Learning with Ease! Your passport to </Typography>
             <Typography>hassle-free attendance management.Let's make </Typography>
             <Typography>every moment count!</Typography>
@@ -63,12 +58,11 @@ const Login = () => {
                   variant="outlined"
                   margin="normal"
                   fullWidth
-                  sx={{ width: '60%', backgroundColor: 'white',height:'2px',marginTop:'12%',marginLeft: '20%' }}
-
+                  sx={{ width: '60%', backgroundColor: 'white',height:'0.1px',marginTop:'12%',marginLeft: '20%' }}
                   InputProps={{
                     endAdornment: (
                           <InputAdornment position='end'>
-                            <img src="../src/assets/mail.png"  alt="Image" style={{ height: '15px', width: '18px'}} />
+                            <img src="../src/assets/mail.png"  alt="Image" style={{ height: '12px', width: '15px'}} />
                           </InputAdornment>
                         ),
                       }}
@@ -80,21 +74,22 @@ const Login = () => {
                   variant="outlined"
                   margin="normal"
                   fullWidth
-                  sx={{ width: '60%', backgroundColor: 'white',height:'1px' ,marginTop: '12%',marginLeft: '20%' }}
-
+                  sx={{ width: '60%', backgroundColor: 'white',height:'0.1px' ,marginTop: '12%',marginLeft: '20%' }}
 
                   InputProps={{
                     endAdornment: (
                           <InputAdornment position="end">
-                            <img src="../src/assets/password.png"  alt="Image" style={{ height: '15px', width: '18px'}} />
+                            <img src="../src/assets/password.png"  alt="Image" style={{ height: '12px', width: '15px'}} />
                           </InputAdornment>
                         ),
                       }}
                 />
 
-                 <Button type="submit" variant="contained" color="primary"  sx={{ width: '50%',height:'50px',marginTop: '120px',marginLeft: '25%' }} >
+                <Button type="submit" variant="contained"  
+                  sx={{ width: '50%',height:'40px',marginTop: '100px',marginLeft: '25%', backgroundColor: '#004AAD',borderRadius:'15px'}} >
                   Login
                 </Button> 
+
 
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px',marginBottom: '30px' }}>
                   <div style={{ margin: '10px 0', width: '30%' }}>
@@ -112,7 +107,7 @@ const Login = () => {
                 
 
 
-                <Button type="submit" variant="contained" color="primary" fullWidth sx={{ backgroundColor: 'white', color: 'black', width: '50%', height: '50px',marginLeft: '25%'  }}>
+                <Button type="submit" variant="contained" color="primary" fullWidth sx={{ backgroundColor: 'white', color: 'black', width: '50%', height: '40px',marginLeft: '25%',borderRadius:'15px',marginBottom:'20px' }}>
                 <img src="../src/assets/google.png" style={{ maxWidth: '100%', maxHeight: '80%' }} /> 
                 <Typography sx={{ color: 'black',fontSize:15 }}>Continue with Google</Typography>
               </Button> 
