ccdd1bffb6e2b63edce3908904aef5c89150b09a
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2024-02-20 14:49:58 +0530

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,20 @@ import Login from "./pages/LogIn";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<Layout />}>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/timetable" element={<Timetable />} />
-      <Route path="/attendance" element={<SessionAttendance />} />
-      <Route path="/updateUser" element={<UpdateUser />} />
-      <Route path="/addlecturer" element={<AddLecturer />} />
-      <Route path="/addstudent" element={<AddStudent />} />
-      <Route path="/login" element={<Login />} />
-    </Route>
+    <>
+      <Route>
+        <Route path="/" element={<Login />} />
+      </Route>
+      <Route element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/timetable" element={<Timetable />} />
+        <Route path="/attendance" element={<SessionAttendance />} />
+        <Route path="/updateUser" element={<UpdateUser />} />
+        <Route path="/addlecturer" element={<AddLecturer />} />
+        <Route path="/addstudent" element={<AddStudent />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Route>
+    </>
   )
 );
 
