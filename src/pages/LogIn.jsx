e620c348e0ad58f36a52e4b1d84397ecd2e0a169
Adib <adibmbrk@gmail.com>
2024-03-16 21:45:09 +0530

    fixed the loading issue in the login page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,6 +1,6 @@
-import { Grid, Typography, Paper } from "@mui/material";
+import { Grid, Typography, Paper, CircularProgress, Box } from "@mui/material";
 import { styled } from "@mui/system";
-import { SignIn } from "@clerk/clerk-react";
+import { ClerkLoaded, SignIn, useAuth } from "@clerk/clerk-react";
 import backgroundImage from "../assets/Group 17.png";
 import leftColumnPaperImage from "../assets/logo.png";
 import leftColumnPaperImageTwo from "../assets/picture.png";
@@ -25,71 +25,97 @@ const RightColumnPaper = styled(Paper)(() => ({
 }));
 
 const Login = () => {
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
   return (
     <div>
-      <Grid container>
-        <Grid
-          item
-          sx={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            height: "50%",
-            bottom: 0,
-            width: "40%",
-          }}
-        >
-          <LeftColumnPaper>
-            <img
-              src={leftColumnPaperImage}
-              alt="Left Column Image"
-              style={{ maxWidth: "70%", maxHeight: "70%", marginTop: "-70px" }}
-            />
-            <img
-              src={leftColumnPaperImageTwo}
-              alt="Left Column Image"
-              style={{ maxWidth: "60%", maxHeight: "60%", marginTop: "-15%" }}
-            />
-            <Typography
-              style={{ fontSize: 20, color: "white", marginTop: "5%" }}
-            >
-              {" "}
-              Streamlining Attendance, Empowering Efficiency!
-            </Typography>
-          </LeftColumnPaper>
-        </Grid>
+      <ClerkLoaded>
+        <Grid container>
+          <Grid
+            item
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              height: "50%",
+              bottom: 0,
+              width: "40%",
+            }}
+          >
+            <LeftColumnPaper>
+              <img
+                src={leftColumnPaperImage}
+                alt="Left Column Image"
+                style={{
+                  maxWidth: "70%",
+                  maxHeight: "70%",
+                  marginTop: "-70px",
+                }}
+              />
+              <img
+                src={leftColumnPaperImageTwo}
+                alt="Left Column Image"
+                style={{ maxWidth: "60%", maxHeight: "60%", marginTop: "-15%" }}
+              />
+              <Typography
+                style={{ fontSize: 20, color: "white", marginTop: "5%" }}
+              >
+                {" "}
+                Streamlining Attendance, Empowering Efficiency!
+              </Typography>
+            </LeftColumnPaper>
+          </Grid>
 
-        {/* Right Column */}
-        <Grid
-          item
-          sx={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            height: "100%",
-            width: "60%",
-            bottom: 0,
-          }}
-        >
-          <RightColumnPaper>
-            <Typography
-              variant="h3"
-              sx={{ fontWeight: "bold", color: "#004AAD", paddingTop: "30px" }}
-            >
-              Welcome Back!
-            </Typography>
-            <Typography>
-              Unlock Learning with Ease! Your passport to{" "}
-            </Typography>
-            <Typography>
-              hassle-free attendance management. Let&rsquo;s make{" "}
-            </Typography>
-            <Typography>every moment count!</Typography>
+          {/* Right Column */}
+          <Grid
+            item
+            sx={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              height: "100%",
+              width: "60%",
+              bottom: 0,
+            }}
+          >
+            <RightColumnPaper>
+              <Typography
+                variant="h3"
+                sx={{
+                  fontWeight: "bold",
+                  color: "#004AAD",
+                  paddingTop: "30px",
+                }}
+              >
+                Welcome Back!
+              </Typography>
+              <Typography>
+                Unlock Learning with Ease! Your passport to{" "}
+              </Typography>
+              <Typography>
+                hassle-free attendance management. Let&rsquo;s make{" "}
+              </Typography>
+              <Typography>every moment count!</Typography>
 
-            <SignIn />
-          </RightColumnPaper>
+              <SignIn />
+            </RightColumnPaper>
+          </Grid>
         </Grid>
-      </Grid>
+      </ClerkLoaded>
     </div>
   );
 };
