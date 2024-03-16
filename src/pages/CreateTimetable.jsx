7ea1bac19bb7f021d06d91017fc98f98e01f4d66
Inoj <inojhettiarachchi@gmail.com>
2024-03-16 17:15:06 +0530

    Made changes to option allowed for level and program when uploading csv

--- src/pages/CreateTimetable.jsx
+++ src/pages/CreateTimetable.jsx
@@ -92,120 +92,16 @@ function CreateTimetable() {
                 value={selectedCourseType}
                 onChange={handleCourseTypeChange}
               >
-                <MenuItem value="Foundation Certificate in Higher Education – Business">
-                  Foundation Certificate in Higher Education – Business
+                <MenuItem value="SE">
+                  SE
                 </MenuItem>
-                <MenuItem value="Foundation Certificate in Higher Education – IT">
-              Foundation Certificate in Higher Education – IT
+                <MenuItem value="CS">
+              CS
             </MenuItem>
-            <MenuItem value="BSc (Hons) Business Information Systems">
-              BSc (Hons) Business Information Systems
+            <MenuItem value="BIS">
+              BIS
             </MenuItem>
-            <MenuItem value="BA (Hons) Business Management">
-              BA (Hons) Business Management
-            </MenuItem>
-            <MenuItem value="BSc (Hons) Business Data Analytics">
-              BSc (Hons) Business Data Analytics
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Digital Graphic Designing">
-              Professional Certificate in Digital Graphic Designing
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Python Programming">
-              Professional Certificate in Python Programming
-            </MenuItem>
-            <MenuItem value="BSc (Hons) Artificial Intelligence And Data Science">
-              BSc (Hons) Artificial Intelligence And Data Science
-            </MenuItem>
-            <MenuItem value="BSc (Hons) Computer Science">
-              BSc (Hons) Computer Science
-            </MenuItem>
-            <MenuItem value="BEng (Hons) Software Engineering">
-              BEng (Hons) Software Engineering
-            </MenuItem>
-            <MenuItem value="MSc Advanced Software Engineering">
-              MSc Advanced Software Engineering
-            </MenuItem>
-            <MenuItem value="MSc Cyber Security And Forensics">
-              MSc Cyber Security And Forensics
-            </MenuItem>
-            <MenuItem value="Professional Certificate in 3D Animation">
-              Professional Certificate in 3D Animation
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Cyber Security & Networking">
-              Professional Certificate in Cyber Security & Networking
-            </MenuItem>
-            <MenuItem value="Finance for Non-Finance Executives">
-              Finance for Non-Finance Executives
-            </MenuItem>
-            <MenuItem value="MSc Big Data Analytics">
-              MSc Big Data Analytics
-            </MenuItem>
-            <MenuItem value="MSc Information Technology">
-              MSc Information Technology
-            </MenuItem>
-            <MenuItem value="MSc Business Analytics">
-              MSc Business Analytics
-            </MenuItem>
-            <MenuItem value="MA Fashion Business Management">
-              MA Fashion Business Management
-            </MenuItem>
-            <MenuItem value="Certified Ethical Hacker (C|EH) – Accredited by EC Council">
-              Certified Ethical Hacker (C|EH) – Accredited by EC Council
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Web Development">
-              Professional Certificate in Web Development
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Machine Learning">
-              Professional Certificate in Machine Learning
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Blockchain">
-              Professional Certificate in Blockchain
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Advanced Cyber Security">
-              Professional Certificate in Advanced Cyber Security
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Foundation Level">
-              Professional Certificate in Software Quality Assurance – Foundation Level
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Information Technology">
-              Professional Certificate in Information Technology
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Network & System Administration">
-              Professional Certificate in Network & System Administration
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Cyber Security">
-              Professional Certificate in Cyber Security
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Java Programming with Object Oriented">
-              Professional Certificate in Java Programming with Object Oriented
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Data Visualisation with Power BI">
-              Professional Certificate in Data Visualisation with Power BI
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Search Engine Optimization (SEO)">
-              Professional Certificate in Search Engine Optimization (SEO)
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Advanced Level">
-              Professional Certificate in Software Quality Assurance – Advanced Level
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Expert Level : Test Management">
-              Professional Certificate in Software Quality Assurance – Expert Level : Test Management
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Expert Level: Test Automation">
-              Professional Certificate in Software Quality Assurance – Expert Level: Test Automation
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Expert Level: Performance Testing">
-              Professional Certificate in Software Quality Assurance – Expert Level: Performance Testing
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Software Quality Assurance – Expert Level: Security Testing">
-              Professional Certificate in Software Quality Assurance – Expert Level: Security Testing
-            </MenuItem>
-            <MenuItem value="Professional Certificate in 3D Character Animation with Maya">
-              Professional Certificate in 3D Character Animation with Maya
-            </MenuItem>
-            <MenuItem value="Professional Certificate in Python for Application Development – Level 2">
-              Professional Certificate in Python for Application Development – Level 2
-            </MenuItem>
+            
               </Select>
             </FormControl>
           </Grid>
@@ -226,7 +122,7 @@ function CreateTimetable() {
               onChange={handleTutorialGroupChange}  
               //commit
             >
-              {['A', 'B', 'C', 'D', 'E'].map((letter) => (
+              {['A', 'B', 'C', 'D'].map((letter) => (
               <MenuItem key={letter} value={letter}>
                 <Typography>{letter}</Typography>
               </MenuItem>
@@ -252,33 +148,12 @@ function CreateTimetable() {
                 value={selectedLevel} 
                 onChange={handleLevelChange} 
               >
-                <MenuItem value="Level 1">Level 1</MenuItem>
-                <MenuItem value="Level 2">
-              Level 2
+                <MenuItem value="L4">L4</MenuItem>
+                <MenuItem value="L5">
+              L5
             </MenuItem>
-            <MenuItem value="Level 3">
-              Level 3
-            </MenuItem>
-            <MenuItem value="Level 4">
-              Level 4
-            </MenuItem>
-            <MenuItem value="Level 5">
-              Level 5
-            </MenuItem>
-            <MenuItem value="Level 6">
-              Level 6
-            </MenuItem>
-            <MenuItem value="Level 7">
-              Level 7
-            </MenuItem>
-            <MenuItem value="Level 8">
-              Level 8
-            </MenuItem>
-            <MenuItem value="Foundation Level">
-              Foundation Level
-            </MenuItem>
-            <MenuItem value="Advance Level">
-              Advance Level
+            <MenuItem value="L6">
+              L6
             </MenuItem>
               </Select>
             </FormControl>
